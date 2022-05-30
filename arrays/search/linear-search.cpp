af1111b31de6129d2109f9451c163c262d4409fe
Kaushikdey647 <kaushikdey647@gmail.com>
2022-05-30 12:58:52 +0530

    added some new search algos

---
+++ arrays/search/linear-search.cpp
@@ -0,0 +1,94 @@
+/*
+ /$$                                 /$$                      /$$$$$$  /$$   /$$ /$$$$$$$$
+| $$                                | $$                     /$$__  $$| $$  | $$|_____ $$/
+| $$   /$$  /$$$$$$  /$$   /$$  /$$$$$$$  /$$$$$$   /$$$$$$ | $$  \__/| $$  | $$     /$$/ 
+| $$  /$$/ |____  $$| $$  | $$ /$$__  $$ /$$__  $$ /$$__  $$| $$$$$$$ | $$$$$$$$    /$$/  
+| $$$$$$/   /$$$$$$$| $$  | $$| $$  | $$| $$$$$$$$| $$$$$$$$| $$__  $$|_____  $$   /$$/   
+| $$_  $$  /$$__  $$| $$  | $$| $$  | $$| $$_____/| $$_____/| $$  \ $$      | $$  /$$/    
+| $$ \  $$|  $$$$$$$|  $$$$$$$|  $$$$$$$|  $$$$$$$|  $$$$$$$|  $$$$$$/      | $$ /$$/     
+|__/  \__/ \_______/ \____  $$ \_______/ \_______/ \_______/ \______/       |__/|__/      
+                     /$$  | $$                                                            
+                    |  $$$$$$/                                                            
+                     \______/                                                             
+
+
+
+Name: Kaushik Dey
+Institute: IIT Bhubaneswar
+Mail: 20cs01043@iitbbs.ac.in
+Linkedin: https://www.linkedin.com/in/kaushik-dey-235531201/
+
+
+    "Everything should be made as simple as possible, But not simpler"	
+
+
+                                                                 - Anonymous
+*/
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <complex>
+#include <unordered_set>
+#include <list>
+#include <queue>
+#include <set>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+
+template<class T>
+T cinget(std::istream& is){
+  T result;
+  is >> result;
+  return result;
+}
+
+template<class T>
+void swap(T*a, T*b){
+T temp = *b;
+*b = *a;
+*a = temp;
+}
+
+bool linearSearch(int* begin, int* end, int X){
+    for(int* ptr = begin; ptr != end; ptr++){
+        if(*ptr == X){
+            return true;
+        }
+    }
+    return false;
+}
+
+void algoChecker(int* begin, int* end, bool(*search)(int*, int*,int)){
+    bool ok = true;
+    for(int* ptr = begin; ptr != end; ptr++){
+        if(!search(begin,end,*ptr)){
+            cout << "Couldn't find element " << *ptr << " at index " << ptr-begin << endl;
+            ok = false;
+        }
+    }
+    if(ok) cout << "All Checks Passed" << endl;
+}
+
+int main()
+{
+    int arr1[] = {1,3,6,9,10,11,124,235,456,586,698,890,1342,3524,6346,5477,5688,6798,7809,23466};
+    int arr2[] = {1,4,7,9,12,42,65,74,86,99,123,234,315,451,512,674,721,893,901};
+    cout << "Array1" << endl;
+    algoChecker(arr1,arr1+20,&linearSearch);
+    cout << "Array2" << endl;
+    algoChecker(arr2,arr2+19,&linearSearch);
+}
---
+++ arrays/sort/heap-sort.cpp
@@ -0,0 +1,103 @@
+/*
+ /$$                                 /$$                      /$$$$$$  /$$   /$$ /$$$$$$$$
+| $$                                | $$                     /$$__  $$| $$  | $$|_____ $$/
+| $$   /$$  /$$$$$$  /$$   /$$  /$$$$$$$  /$$$$$$   /$$$$$$ | $$  \__/| $$  | $$     /$$/ 
+| $$  /$$/ |____  $$| $$  | $$ /$$__  $$ /$$__  $$ /$$__  $$| $$$$$$$ | $$$$$$$$    /$$/  
+| $$$$$$/   /$$$$$$$| $$  | $$| $$  | $$| $$$$$$$$| $$$$$$$$| $$__  $$|_____  $$   /$$/   
+| $$_  $$  /$$__  $$| $$  | $$| $$  | $$| $$_____/| $$_____/| $$  \ $$      | $$  /$$/    
+| $$ \  $$|  $$$$$$$|  $$$$$$$|  $$$$$$$|  $$$$$$$|  $$$$$$$|  $$$$$$/      | $$ /$$/     
+|__/  \__/ \_______/ \____  $$ \_______/ \_______/ \_______/ \______/       |__/|__/      
+                     /$$  | $$                                                            
+                    |  $$$$$$/                                                            
+                     \______/                                                             
+
+
+
+Name: Kaushik Dey
+Institute: IIT Bhubaneswar
+Mail: 20cs01043@iitbbs.ac.in
+Github: https://github.com/Kaushikdey647?tab=repositories
+Twitter: https://twitter.com/KayDee647
+
+
+    "Everything should be made as simple as possible, But not simpler"	
+
+
+                                                                 - Anonymous
+*/
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <complex>
+#include <unordered_set>
+#include <list>
+#include <queue>
+#include <set>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+
+template<class T>
+T cinget(std::istream& is){
+  T result;
+  is >> result;
+  return result;
+}
+
+template<class T>
+void swap(T*a, T*b){
+T temp = *b;
+*b = *a;
+*a = temp;
+}
+
+void heapify(int* arr, int n, int i){
+    int largest = i; //say root
+    int l = 2*i + 1; //lchild
+    int r = 2*i + 2; //rchild
+    if(l < n && arr[l] > arr[largest]) largest = l;
+    if(r < n && arr[r] > arr[largest]) largest = r;
+    if(largest != i){
+        swap<int>(arr+i,arr+largest);
+        heapify(arr,n,largest);
+    }
+}
+
+bool heapSort(int* begin, int* end){
+    int n = end - begin;
+    try{
+        for(int i = n/2-1; i >= 0; i--){
+            heapify(begin,n,i);
+        }
+        for(int j = n-1; j > 0; j--){
+            swap(begin,begin+j);
+            heapify(begin,j,0);
+        }
+    }
+    catch(...){
+        return false;
+    }
+    return true;
+}
+
+int main()
+{
+    int arr[] = {54,346,478,347,554,1634,6134,2134,879,35,568,135}; //12 ELEMS
+    heapSort(arr,arr+12);
+    for(int i = 0; i < 12; i++){
+        cout << arr[i] <<" ";
+    }
+}
