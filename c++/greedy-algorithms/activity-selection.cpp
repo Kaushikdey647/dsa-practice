e4cea1bc47f400de9719448d9a7f8d404f015273
Kaushikdey647 <Kaushikdey647@gmail.com>
2022-07-17 01:40:10 +0530

    Restructured

---
+++ c++/greedy-algorithms/activity-selection.cpp
@@ -0,0 +1,104 @@
+/*
+ /$$                                 /$$                      /$$$$$$  /$$   /$$ /$$$$$$$$
+| $$                                | $$                     /$$__  $$| $$  | $$|_____ $$/
+| $$   /$$  /$$$$$$  /$$   /$$  /$$$$$$$  /$$$$$$   /$$$$$$ | $$  \__/| $$  | $$     /$$/ 
+| $$  /$$/ |____  $$| $$  | $$ /$$__  $$ /$$__  $$ /$$__  $$| $$$$$$$ | $$$$$$$$    /$$/  
+| $$$$$$/   /$$$$$$$| $$  | $$| $$  | $$| $$$$$$$$| $$$$$$$$| $$__  $$|_____  $$   /$$/   
+| $$_  $$  /$$__  $$| $$  | $$| $$  | $$| $$_____/| $$_____/| $$  \ $$      | $$  /$$/    
+| $$ \  $$|  $$$$$$$|  $$$$$$$|  $$$$$$$|  $$$$$$$|  $$$$$$$|  $$$$$$/      | $$ /$$/     
+|__/  \__/ \_______/ \____  $$ \_______/ \_______/ \_______/ \______/       |__/|__/      
+                     /$$  | $$                                                            
+                    |  $$$$$$/                                                            
+                     \______/                                                             
+
+
+
+Name: Kaushik Dey
+Institute: IIT Bhubaneswar
+Mail: 20cs01043@iitbbs.ac.in
+Github: https://github.com/Kaushikdey647?tab=repositories
+Twitter: https://twitter.com/KayDee647
+
+
+    "Everything should be made as simple as possible, But not simpler"	
+
+
+                                                                 - Anonymous
+*/
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#include <complex>
+#include <unordered_set>
+#include <list>
+#include <queue>
+#include <set>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <stack>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+
+template<class T>
+T cinget(std::istream& is){
+  T result;
+  is >> result;
+  return result;
+}
+
+template<class T>
+void swap(T*a, T*b){
+T temp = *b;
+*b = *a;
+*a = temp;
+}
+
+vector<pair<int, int>> activitySelection(vector<pair<int, int>> activities)
+{
+  //Vectors are sorted in finish time
+  sort(
+    activities.begin(),
+    activities.end(),
+    [](const pair<int, int>& a, const pair<int, int>& b){
+      return a.second < b.second;
+    }
+  );
+  vector<pair<int, int>> res;
+
+  auto selected = activities.begin(); // First Element always in
+
+  res.push_back(*selected);
+
+  for (auto itr = activities.begin() + 1; itr != activities.end(); itr++)
+  {
+    //Check the upcoming one
+    if( itr-> first >= selected -> second ){
+      res.push_back(*itr);
+      selected = itr;
+    }
+  }
+
+  return res;
+}
+
+int main()
+{
+  vector<pair<int, int>> input{ {10,20},{12,25},{20,40}};
+  vector<pair<int, int>> output = activitySelection(input);
+  for (auto pair : output)
+  {
+    cout << " | " << pair.first << " , " << pair.second;
+  }
+  cout << " | " << endl;
+}
